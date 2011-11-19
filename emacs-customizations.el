787c244a253d5c090a1fb129603f739e999ad1f5
stask <stask@me.com>
2011-11-19 17:07:02 +0200

    added some initial customizations

---
+++ emacs-customizations.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(ido-enable-flex-matching t)
+ '(ido-mode (quote both) nil (ido))
+ '(mark-even-if-inactive nil)
+ '(org-emphasis-alist (quote (("*" bold "<strong>" "</strong>") ("/" italic "<em>" "</em>") ("_" underline "<span style=\"text-decoration:underline;\">" "</span>") ("=" org-code "<code>" "</code>" verbatim) ("~" org-verbatim "<code>" "</code>" verbatim) ("+" (:strike-through t) "<del>" "</del>"))))
+ '(org-export-htmlize-output-type (quote css))
+ '(org-export-htmlized-org-css-url nil)
+ '(org2blog/wp-default-categories (quote ("Uncategorized")))
+ '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("marmalade" . "http://marmalade-repo.org/packages/") ("Tromey" . "http://tromey.com/elpa/"))))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init.el
+++ init.el
@@ -1,1 +1,24 @@
 ;; emacs configuration
+;; inspired by Avdi Grimm's config
+
+;; Directories and file names
+(setq stask-emacs-init-file (or load-file-name buffer-file-name))
+(setq stask-emacs-config-dir
+      (file-name-directory stask-emacs-init-file))
+(setq user-emacs-directory stask-emacs-config-dir)
+(setq stask-elisp-dir (expand-file-name "elisp" stask-emacs-config-dir))
+(setq stask-elisp-external-dir
+      (expand-file-name "external" stask-elisp-dir))
+(setq stask-init-dir
+      (expand-file-name "init.d" stask-emacs-config-dir))
+
+;; Load all elisp files in ./init.d
+(if (file-exists-p stask-init-dir)
+    (dolist (file (directory-files stask-init-dir t "\\.el$"))
+      (load file)))
+
+;; Setup 'custom' system
+(setq custom-file
+      (expand-file-name "emacs-customizations.el" stask-emacs-config-dir))
+(load custom-file)
+
