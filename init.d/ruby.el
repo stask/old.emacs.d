22361ae5a28f15ea3f5dfe02296ffe9b230fe683
stask <stask@me.com>
2011-12-07 14:53:59 +0200

    added rhtml mode

--- init.d/ruby.el
+++ init.d/ruby.el
@@ -15,3 +15,6 @@
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
+
+(require 'rhtml-mode)
